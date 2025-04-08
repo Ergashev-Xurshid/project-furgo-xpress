e8e37bb9de8d15ecb53ac270ae48d6b4986937a6
Ergashev-Xurshid <xurshidgithub@gmail.com>
2025-04-08 17:02:48 +0500

    add services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { servicesItem } from '../utils/constanta'
+import { FaArrowRight } from "react-icons/fa";
 
 function Services() {
   return (
-    <section className='py-[95px] container bg-[#f5f5f5] px-[10px] sm:px-[15px] md:px-[100px] lg:px-[80px] mx-auto'>
+    <section className=' py-[95px] container bg-[#f5f5f5] px-[10px] sm:px-[15px] md:px-[50px] lg:px-[80px] mx-auto'>
           <div className='mx-auto text-center w-full lg:w-[50%]'>
             <h1 className='text-[36px] mb-[21px] uppercase text-black font-bold'>our services</h1>
             <span className='inline-block w-[70px] h-[3px] bg-[#3db7ef]'></span>
@@ -11,13 +12,17 @@ function Services() {
               Our transportation company has been offering a vast set of reliable solutions for businesses and individuals all over the world since our establishment.
             </p>
           </div>
-          <div>
+          <div className='flex justify-center  gap-10 flex-wrap mt-[50px] '>
             { servicesItem.map((item,i)=>(
-              <div key={i}>
-                <img src={item.image} alt={item.title} />
-                <div className='label-shape bg-[#3db7ef] text-white px-6 py-2 text-lg font-medium w-fit'>
-                  <i className='hidden'></i>
+              <div key={i} className='relative hover-item cursor-pointer w-[90%] md:w-[45%] lg:w-[30%] h-[250px] overflow-hidden'>
+                <img className='w-full h-full object-cover' src={item.image} alt={item.title} />
+                <div className=' absolute bottom-[20px] left-[-20px] transition-all duration-300 label-shape bg-[#3db7ef] text-white px-[40px]  text-lg font-medium w-fit'>
+                  <div className='flex items-center gap-2'>
+                    <span className='inline-block opacity-0  transition-all duration-300 '>
+                    <FaArrowRight />
+                    </span>
                   <h3 className='font-normal text-[20px] text-white capitalize'>{item.title}</h3>
+                  </div>
                 </div>
               </div>
             ))
--- src/utils/constanta.js
+++ src/utils/constanta.js
@@ -48,7 +48,7 @@ export const HomeTitle = [
 import img1 from "../assets/img1.png"
 import img2 from "../assets/img2.png"
 import img3 from "../assets/img3.png"
-import img4 from "../assets/img4.png"
+import img_4 from "../assets/img4.jpg"
 import img5 from "../assets/img5.png"
 import img6 from "../assets/img6.png"
 
@@ -67,7 +67,7 @@ export const servicesItem = [
     title:"Towing Service"
   },
   {
-    image:img4,
+    image:img_4,
     title:"Maintenance Leasing"
   },
   {
